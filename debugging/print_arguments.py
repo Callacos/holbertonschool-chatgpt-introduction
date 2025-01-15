8fc3e1db01691af6eea61c3207aff6557cde791b
Callacos <callacos74@gmail.com>
2025-01-15 13:11:23 +0100

    Remove usage check from print_arguments script for streamlined execution

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-if len(sys.argv) < 2:
-    print("Usage: python script.py arg1 arg2 ...")
-    sys.exit(1)
-
 for i in range(1, len(sys.argv)):
     print(sys.argv[i])
-
